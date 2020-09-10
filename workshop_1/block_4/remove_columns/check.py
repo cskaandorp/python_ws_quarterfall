4762baf06111240da0d9b3b49a0746809767f0c5
Casper Kaandorp <c.s.kaandorp@uu.nl>
2020-09-10 16:40:31 +0200

    test

--- workshop_1/block_4/remove_columns/check.py
+++ workshop_1/block_4/remove_columns/check.py
@@ -1,7 +1,7 @@
 import unittest
 import json
 import os.path
-import answer
+from answer import *
 
 
 # Quarterfall object containing the data
@@ -12,7 +12,7 @@ class TestStringMethods(unittest.TestCase):
     # test to check whether the power is correctly computed
     def test_existance(self):
         try:
-            self.assertNotEqual(answer.training != None)
+            self.assertNotEqual(training != None)
             qf['trainingExists'] = 'A'
         except:
             qf['trainingExists'] = 'B'
