a88fd9c11e69527baddd7fe82614009d0da0a2ae
Casper Kaandorp <c.s.kaandorp@uu.nl>
2020-09-10 16:32:16 +0200

    test

--- workshop_1/block_4/remove_columns/check.py
+++ workshop_1/block_4/remove_columns/check.py
@@ -1,21 +1,35 @@
+import unittest
 import json
 import os.path
 import answer
 
 
-if __name__ == '__main__':
+# Quarterfall object containing the data
+qf = {}
 
-    # Quarterfall object containing the data
-    qf = {}
+class TestStringMethods(unittest.TestCase):
 
-    if os.path.exists('qf.json'):
-        json_file = open('qf.json')
-        qf = json.load(json_file)
+    # test to check whether the power is correctly computed
+    def test_existance(self):
+        try:
+            self.assertNotEqual(answer.training != None)
+            qf['trainingExists'] = True
+        except:
+            qf['trainingExists'] = False
+            raise
+    
+    @classmethod
+    def setUpClass(cls):
+        # read the data from QF json file if it exists
+        if os.path.exists('qf.json'):
+            json_file = open('qf.json')
+            qf = json.load(json_file)
 
-        qf['test'] = 'LIJP'
-
+    @classmethod
+    def tearDownClass(cls):
+        # write the qf data to the json file
         outfile = open('qf.json', 'w')
         json.dump(qf, outfile)
 
-
-
+if __name__ == '__main__':
+    unittest.main()
